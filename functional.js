0b95bd2202d94506af06505497fa114392686594
Code Monkey <code.monkey@codemonkey.com>
2019-02-15 16:27:09 -0500

    Prevent Jekyll build

--- functional.js
+++ functional.js
@@ -1,12 +1,29 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
 let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
-//urls: Imperative version
-function imperativeUrls(elements) {
-  let urls = [];
-  elements.forEach(function(element) {
-    urls.push(element.toLowerCase().split(/\s+/).join("-"));
-  });
-  return urls;
+//Imperative version
+function imperative(elements) {
+  let lengths = {};
+  elements.forEach(element => lengths[element] = element.length);
+  return lengths;
 }
 
-console.log(imperativeUrls(states));fngvongv
+console.log(imperative(states));
+
+
+// Functional version
+function functional(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+
+console.log(functional(states));
+
+// Excercise
+function exercise(elements) {
+  return elements.reduce((total, n) => { return total *= n; }, 1);
+}
+
+console.log(exercise(numbers));
--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,56 @@
 //Reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+String.prototype.reverse = function () {
+  return Array.from(this).reverse().join("");
 }
-//Returns true for a palindrome, false otherwise.  ignoreCase true allows for ignoring case.
-function palindrome(string,ignoreCase) {
-  if (ignoreCase) {
-    string = string.toLowerCase();
+
+String.prototype.blank = function () {
+  if (this.match(/^\s*$/)) {
+    return true;
+  } else {
+    return false;
   }
-  return string === reverse(string);
 }
+
+Array.prototype.last = function () {
+  return this.slice(-1);
+}
+
+//Define  a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  //process string to toLowerCase.
+  this.processor = function (string) {
+    return string.toLowerCase();
+  }
+
+  //Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+
+  //Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+//Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+TranslatedPhrase.prototype = new Phrase();
+
+phrase = new Phrase("Racecar");
+console.log(phrase.content);
+console.log(phrase.palindrome());
+
+frase = new TranslatedPhrase("recognize","reconocer");
+console.log(frase);
+console.log(frase.palindrome());
