10d459b874550b08542b15f1606046e9a00b43f3
Code Monkey <code.monkey@codemonkey.com>
2019-02-15 16:53:14 -0500

    on 9.3 Dynamic HTML

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 let Phrase = require("mulder-brewing-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
+function palindromeTester () {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
 
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  // let button = document.querySelector("#palindromeTester");
+  // button.addEventListener("click", palindromeTester);
+  let form = document.querySelector("#palindromeTester");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+  })
+})
