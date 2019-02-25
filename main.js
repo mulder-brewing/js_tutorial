a524d915f2ac97debc102508bd02c065804c4282
Code Monkey <code.monkey@codemonkey.com>
2019-02-25 13:18:07 -0500

    on chapter 10

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 let Phrase = require("mulder-brewing-palindrome");
 
-function palindromeTester () {
-  let string = prompt("Please enter a string for palindrome testing:");
-  let phrase = new Phrase(string);
+function palindromeTester (event) {
+  event.preventDefault();
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+      palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+      palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  // let button = document.querySelector("#palindromeTester");
-  // button.addEventListener("click", palindromeTester);
-  let form = document.querySelector("#palindromeTester");
-  form.addEventListener("submit", function() {
-    palindromeTester();
-  })
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
 })
